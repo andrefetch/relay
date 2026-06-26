4006f9635235e263fe8f9915a2303dfc7eb80620
andrefetch <andresilvapersonal2007@gmail.com>
2026-06-25 23:13:42 -0400

    Fixed: spelling errors in imports.

--- client/llm_client.py
+++ client/llm_client.py
@@ -1,14 +1,14 @@
-from openai import AysncOpenAI
+from openai import AsyncOpenAI
 from typing import Any
 
 class LLMClient:
     def __init__(self) -> None:
-        self._client : AysncOpenAI | None = None
+        self._client : AsyncOpenAI | None = None
 
-    def get_client(self) -> AysncOpenAI:
+    def get_client(self) -> AsyncOpenAI:
         if self._client is None:
-            self._client = AysncOpenAI(
-                    api_key="",
+            self._client = AsyncOpenAI(
+                    api_key="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",
                     base_url="https://openrouter.ai/api/v1",
             )
         return self._client
@@ -19,13 +19,23 @@ class LLMClient:
             self._client = None
 
     async def chat_completion(self, messages: list[dict[str, Any]], stream: bool = True):
+
+        client = self.get_client()
+
+        kwargs = {
+            "model": "nvidia/nemotron-3-ultra-550b-a55b:free",
+            "messages": messages,
+            "stream": stream,
+        }
+
         if stream:
             self._stream_response()
         else:
-            self.non_stream_response()
+            self.non_stream_response(client, kwargs)
         
     async def _stream_response(self):
         pass
 
-    async def _non_stream_response(self):
-        pass
+    async def _non_stream_response(self, client: AsyncOpenAI, kwargs: dict[str, Any]):
+        response = await client.chat.completions.create(**kwargs)
+        print(response)
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('hello world')
+from client.llm_client import LLMClient
+
+async def main():
+
+    client = LLMClient()
+    messages = [{
+        'role': 'user',
+        'content': 'what is up'
+    }]
+    await client.chat_completion(messages, False)
+    print('done')
