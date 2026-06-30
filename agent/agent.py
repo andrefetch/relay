41223aaa4c2871c886bccc59b7acbf4fc3e21b45
andrefetch <andresilvapersonal2007@gmail.com>
2026-06-30 17:42:07 -0400

    feat: agent class for start of agentic loop, can only do one message rn

--- agent/agent.py
+++ agent/agent.py
@@ -1,3 +1,37 @@
+from typing import AsyncGenerator
+from agent.events import AgentEvent, AgentEventType
+from client.llm_client import LLMClient
+from client.response import StreamEventType
+
 class Agent:
     def __init__(self):
-        pass
+        self.client = LLMClient()
+    
+    async def run(self, message: str):
+        yield AgentEvent.agent_start(message)
+        # add user message to context
+
+        async for event in self._agentic_loop():
+            yield event
+
+            if event.type == AgentEventType.TEXT_COMPLETE:
+                final_response = event.data.get("content")
+        
+        yield AgentEvent.agent_end()
+    
+    async def _agentic_loop(self) -> AsyncGenerator[AgentEvent, None]:
+
+        messages = [{"role": "user", "content": "Hey, what is going on?"}]
+
+        response_text = ""
+        
+        async for event in self.client.chat_completion(messages, True):
+            if event.type == StreamEventType.TEXT_DELTA:
+                content = event.text_delta.content
+                response_text += content
+                yield AgentEvent.text_delta(content)
+            elif event.type == StreamEventType.ERROR:
+                yield AgentEvent.agent_error(event.error or "Unknown error occured.")
+    
+        if response_text:
+            yield AgentEvent.text_complete(response_text)
