607fd4c1a81bcce4ab7a861641665f424e19bb14
andrefetch <andresilvapersonal2007@gmail.com>
2026-07-01 13:05:41 -0400

    feat: validate params method added into the tool invoke

--- tools/base.py
+++ tools/base.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 import abc
 from pathlib import Path
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 from dataclasses import dataclass, field
 from enum import Enum
 from typing import Any
 
-class ToolType(str, Enum):
+class ToolClass(str, Enum):
     READ = "read"
     WRITE = "write"
     SHELL = "shell"
@@ -29,7 +29,7 @@ class ToolInvocation:
 class Tool(abc.ABC):
     name: str = "base_tool"
     description: str = "Base tool"
-    type: ToolType = ToolType.READ
+    type: ToolClass = ToolClass.READ
 
     def __init__(self) -> None:
         pass
@@ -39,4 +39,21 @@ class Tool(abc.ABC):
         raise NotImplementedError("Error: Tool must defined a schema property or an attribute.")
 
     @abc.abstractclassmethod
-    async def execute(self, invocation: ToolInvocation) -> ToolResult
+    async def execute(self, invocation: ToolInvocation) -> ToolResult:
+        pass
+
+    def validate_params(self, params: dict[str, Any]) -> list[str]:
+        schema = self.schema
+        if isinstance(schema, type) and issubclass(schema, BaseModel):
+            try:
+                schema(**params)
+            except ValidationError as e:
+                errors = []
+                for error in e.errors():
+                    ".".join(str(x) for x in error.get("loc", []))
+                    msg = error.get('msg', 'Validation Error')
+                    errors.append(f"Paramater: '{field}': {msg}")
+                
+                return errors
+            except Exception as e:
+                return [str(e)]
